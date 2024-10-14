1a162c0a765ff30c0bd7c983e7c45445c536c638
yyyzz23570 <tv23570@bristol.ac.uk>
2024-10-14 18:28:03 +0100

    AWS1

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -20,12 +21,22 @@ func readFromServer(conn net.Conn) {
 }
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8030")
+	// 使用 flag 包从命令行参数中读取服务器 IP 和端口
+	serverIP := flag.String("server-ip", "127.0.0.1", "Server IP address")
+	port := flag.String("port", "8030", "Server port")
+	flag.Parse()
+
+	// 拼接服务器地址
+	address := *serverIP + ":" + *port
+
+	conn, err := net.Dial("tcp", address)
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
 		os.Exit(1)
 	}
 	defer conn.Close()
+
+	fmt.Println("Connected to server at", address)
 
 	go readFromServer(conn)
 
